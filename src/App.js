dd800b608522983dc27c5ac2991f790181306594
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-02 13:25:58 +0300

    added start up files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import './App.css';
-import Home from './pages/Home';
-import Order from './pages/Order';
-import About from './pages/About';
-import Saved from './pages/Saved';
+import "./App.css";
+import Home from "./pages/Home";
+import Order from "./pages/Order";
+import About from "./pages/About";
+import Saved from "./pages/Saved";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <About />
-      <Order/>
-      <Saved/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={  <Home />} />
+          <Route path="/about" element={  <About />} />
+          <Route path="/order" element={  <Order />} />
+          <Route path="/saved" element={  <Saved />} />
+
+        </Routes>
+        
+      </Router>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Navbar() {
+  const [isOpen , setIsOpen] = useState(false)
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <span className="nav-logo">Didis house</span>
+      <div className={`nav-items ${isOpen && "open"}`}>
+        <Link to="/">Home</Link>
+        <Link to="/order">Order</Link>
+        <Link to="/saved">Saved</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <div className={`nav-toggle ${isOpen && "open"}`}>
+        <div className="bar" onClick={()=>setIsOpen(!isOpen)}></div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
