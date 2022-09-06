4301c55f301a872f38c66c75c0d179544c98ede9
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-06 19:23:30 +0300

    Added select flavour selector

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { cakes } from "../cakedatabase";
 
 function Order() {
- 
-  const displayCakesHome = cakes.map((cake) => (
+  const [flavor , setFlavor]= useState("")
+  const flavorSelector = <select name="cars" value={flavor} onChange={(e)=>{
+    setFlavor(e.target.value)
+  }}  key="flavor selector" id="cars">
+      <option value="">All Flavors</option>
+    <option value="classic">Classics</option>
+  <option value="Forests">Forest Cakes</option>
+  <option value="Fruity">Fruity Cakes</option>
+  <option value="Chocolate">Chocolate Flavors</option>
+
+</select>
+const displayCakes = cakes.map(cake=>{
+  if(cake.type === flavor){
+   return(
     <Link
-      key={cake.id}
-      className="each-ordered-cake-div"
-      to={`/order/${cake.id}`}
-    >
-      <img className="each-cake-image" src={cake.image} alt="cake" />
-      
-      <p className="each-cake-name">{cake.name}</p>
-    </Link>
-  ));
+        key={cake.id}
+        className="each-ordered-cake-div"
+        to={`/order/${cake.id}`}
+      >
+        <img className="each-cake-image" src={cake.image} alt="cake" />
+        
+        <p className="each-cake-name">{cake.name}</p>
+      </Link>
+   )
+
+  }
+  else if(flavor===""){
+    return(
+      <Link
+          key={cake.id}
+          className="each-ordered-cake-div"
+          to={`/order/${cake.id}`}
+        >
+          <img className="each-cake-image" src={cake.image} alt="cake" />
+          
+          <p className="each-cake-name">{cake.name}</p>
+        </Link>
+
+    )
+  }
+})
+ 
   return (
     <div>
       Click on the cake you would love to order
-      <div className="all-cakes-order-div">{displayCakesHome}</div>
+      {flavorSelector}
+      <div className="all-cakes-order-div">{displayCakes}</div>
     </div>
   );
 }
