75d6af81eb0999592ea674f9074915c648cc127c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-07 06:32:32 +0300

    Added about well

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,13 @@
 
 import React  from 'react';
-import Typed from 'react-typed';
+
 function About() {
  
   return (
     <div>
-  
-    <span>
-      CAKES SHOULD BE
-    </span>
-    <span>
-     {"  "}
-    </span>
-
-    <Typed
-    strings={[
-        'tasty',
-        'sweet',
-        'From Didis cake House']}
-        typeSpeed={40}
-        backSpeed={50}
-        
-        loop >
-        
-    </Typed>
-</div>
+      About us
+    </div>
+   
   )
 }
 
