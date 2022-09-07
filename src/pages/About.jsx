ed4e819c61ae13b5f2b9694811cde8a045533058
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-07 06:31:43 +0300

    Added typed js

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,30 +1,30 @@
 
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-
+import React  from 'react';
+import Typed from 'react-typed';
 function About() {
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_8nuxsw3', 'template_4ebr03g', form.current, '6M-rV1iiaVqa5DANh')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
+ 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-    <label>Name</label>
-    <input type="text" name="user_name" />
-    <label>Email</label>
-    <input type="email" name="user_email" />
-    <label>Message</label>
-    <textarea name="message" />
-    <input type="submit" value="Send" />
-  </form>
+    <div>
+  
+    <span>
+      CAKES SHOULD BE
+    </span>
+    <span>
+     {"  "}
+    </span>
+
+    <Typed
+    strings={[
+        'tasty',
+        'sweet',
+        'From Didis cake House']}
+        typeSpeed={40}
+        backSpeed={50}
+        
+        loop >
+        
+    </Typed>
+</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,33 @@ import Classics from "../components/Classics";
 import Chocolate from "../components/Chocolate";
 import Forests from "../components/Forests";
 import Fruity from "../components/Fruity";
+import Typed from 'react-typed';
 function Home() {
+  const cakesTyped =  <div>
+  
+  <span>
+    CAKES SHOULD BE
+  </span>
+  <span>
+   {"  "}
+  </span>
+
+  <Typed
+  strings={[
+      'tasty',
+      'sweet',
+      'From Didis cake House']}
+      typeSpeed={40}
+      backSpeed={50}
+      
+      loop >
+      
+  </Typed>
+</div>
   return (
     <div className="home-div">
       <div className="quote-div">
-        <p>For goodness sake, eat some cake!</p>
-        <p>--vananenae</p>
+      {cakesTyped}
       </div>
       <div className="all-cakes-home-div">
         <Classics />
