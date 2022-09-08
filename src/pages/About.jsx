d8768245f7b7794a2894ddd2e25a52f1bb250b8b
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-09-08 14:48:32 +0300

    styl 5

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,15 @@
 
 import React  from 'react';
-import { toast, ToastContainer } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css'
 
 function About() {
-  const notify=()=>{
-    toast.success('🦄 Wow so easy!', {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      });
-  }
- 
+
+
+  
  
   return (
     <div>
-      <button onClick={notify}>notify</button>
-      <ToastContainer/>
+      
+   
     
     </div>
    
